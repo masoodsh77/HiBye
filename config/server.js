22363caeec4f9242a1b6ce044edc675af11459fc
m.shaterabadi <masoodsh77@gmail.com>
2022-01-08 17:31:01 +0330

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4685bc14de1ebcbe9cfdc159ef607e8d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '43a7b199-224b-4f13-83cb-a3e9cf353cb8'
+};
